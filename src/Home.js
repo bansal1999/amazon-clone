82facf21e3e5eee8e105cd667600f853004e2034
ShikharBansal <shikharbansal007@gmail.com>
2021-02-24 23:03:14 +0530

    Added home page

--- src/Home.js
+++ src/Home.js
@@ -1,24 +1,70 @@
-import React from 'react'
-import './Home.css';
+import React from "react";
+import "./Home.css";
+import Product from "./Product";
 
 function Home() {
-    return (
-        <div className="Home">
-            <div className="home__container">
-                <img className="home__image" 
-                src="https://fortheloveblog.com/wp-content/uploads/2016/07/Amazon-Prime-Banner.jpg" 
-                alt="" />
+  return (
+    <div className="Home">
+      <div className="home__container">
+        <img
+          className="home__image"
+          src="https://fortheloveblog.com/wp-content/uploads/2016/07/Amazon-Prime-Banner.jpg"
+          alt=""
+        />
 
-                <div className="home__row">
-                    {/* product */}
-                    {/* product */}
-                    
-                </div>
-            </div>
-
-
+        <div className="home__row">
+          <Product
+            id="12321341"
+            title="The lean startup"
+            price={29.99}
+            image="https://images-na.ssl-images-amazon.com/images/I/81jgCiNJPUL.jpg"
+            rating={3}
+          />
+          <Product
+            id="12321341"
+            title="The lean startup"
+            price={29.99}
+            image="https://images-na.ssl-images-amazon.com/images/I/81jgCiNJPUL.jpg"
+            rating={3}
+          />
         </div>
-    )
+
+        <div className="home__row">
+          <Product
+            id="12321341"
+            title="The lean startup"
+            price={29.99}
+            image="https://images-na.ssl-images-amazon.com/images/I/81jgCiNJPUL.jpg"
+            rating={3}
+          />
+          <Product
+            id="12321341"
+            title="The lean startup"
+            price={29.99}
+            image="https://images-na.ssl-images-amazon.com/images/I/81jgCiNJPUL.jpg"
+            rating={3}
+          />
+          <Product
+            id="12321341"
+            title="The lean startup"
+            price={29.99}
+            image="https://images-na.ssl-images-amazon.com/images/I/81jgCiNJPUL.jpg"
+            rating={3}
+          />
+        </div>
+
+        <div className="home__row">
+          <Product
+            id="12321341"
+            title="The lean startup"
+            price={29.99}
+            image="https://images-na.ssl-images-amazon.com/images/I/81jgCiNJPUL.jpg"
+            rating={3}
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
