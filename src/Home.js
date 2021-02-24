8dd0178123cf8a1e433d4f2a2e2ee8f5c459af39
ShikharBansal <shikharbansal007@gmail.com>
2021-02-24 21:18:06 +0530

    added header

---
+++ src/Home.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import './Home.css';
+
+function Home() {
+    return (
+        <div className="Home">
+            <div className="home__container">
+                <img className="home__image" 
+                src="https://fortheloveblog.com/wp-content/uploads/2016/07/Amazon-Prime-Banner.jpg" 
+                alt="" />
+
+                <div className="home__row">
+                    {/* product */}
+                    {/* product */}
+                    
+                </div>
+            </div>
+
+
+        </div>
+    )
+}
+
+export default Home
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyB3PYaYHYtYrE2rFu_h1H6H5ghR5TiMSJ8",
+    authDomain: "clone-69d96.firebaseapp.com",
+    projectId: "clone-69d96",
+    storageBucket: "clone-69d96.appspot.com",
+    messagingSenderId: "73476938685",
+    appId: "1:73476938685:web:bf55c563349316ec7590d4",
+    measurementId: "G-K3JQJ04E3P"
+  };
