1e8854ef4f06ca79f3da624a27b4f07780eb1d02
ShikharBansal <shikharbansal007@gmail.com>
2021-02-25 17:18:04 +0530

    completed the checkout page with full functionality

--- src/Product.js
+++ src/Product.js
@@ -1,17 +1,16 @@
 import React from "react";
 import "./Product.css";
-import {useStateValue} from "./StateProvider";
+import { useStateValue } from "./StateProvider";
 
 function Product({ id, title, image, price, rating }) {
+  const [{ basket }, dispatch] = useStateValue();
 
-  const [{basket}, dispatch] = useStateValue();
-
-  console.log("This is the basket>>>>>>" , basket);
+  console.log("This is the basket>>>>>>", basket);
 
   const addToBasket = () => {
     //dispatch action to send the item to the data layer
     dispatch({
-      type: 'ADD_TO_BASKET',
+      type: "ADD_TO_BASKET",
       item: {
         id: id,
         title: title,
--- src/StateProvider.js
+++ src/StateProvider.js
@@ -1,13 +1,12 @@
-import React, {createContext, useContext,  useReducer} from "react";
+import React, { createContext, useContext, useReducer } from "react";
 //Prepares the dataLayer
 export const StateContext = createContext();
 
 //Wrap our app and provide the DATA LAYER
-export const StateProvider = ({reducer, initialState, children}) => (
-    <StateContext.Provider value = {useReducer(reducer, initialState)} >
-        {children}
-    </StateContext.Provider>
-
+export const StateProvider = ({ reducer, initialState, children }) => (
+  <StateContext.Provider value={useReducer(reducer, initialState)}>
+    {children}
+  </StateContext.Provider>
 );
 
 //Pull information from the data layer
